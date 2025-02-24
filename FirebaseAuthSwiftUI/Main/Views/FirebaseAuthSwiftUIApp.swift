5b732815a85070a764f5442003c081b568939877
Nihal <>
2025-02-24 22:50:32 +0500

    first commit

---
+++ FirebaseAuthSwiftUI/Main/Views/FirebaseAuthSwiftUIApp.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct FirebaseAuthSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject private var authViewModel = AuthViewModel()
+    @ObservedObject private var router = Router()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack(path: $router.navPath) {
+                ContentView()
+                    .navigationDestination(for: Router.AuthFlow.self) { destination in
+                        switch destination {
+                        case .login: LoginView()
+                        case .createAccount: CreateAccountView()
+                        case .profile: ProfileView()
+                        case .forgotPassword: ForgotPasswordView()                                
+                        case .emailSent: EmailSentView()
+                        }
+                    }
+            }
+            .environmentObject(authViewModel)
+            .environmentObject(router)
+        }
+    }
+}
